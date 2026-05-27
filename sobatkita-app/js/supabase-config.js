b1f815d5cf790cac622110ba6c90f91d6e28bc05
gomesnopes <fandigomes.abk@gmail.com>
2026-05-27 20:40:19 +0900

    Update supabase-config.js

--- sobatkita-app/js/supabase-config.js
+++ sobatkita-app/js/supabase-config.js
@@ -1,7 +1,8 @@
+
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // GANTI DENGAN KREDENSIAL SUPABASE ANDA
-const supabaseUrl = 'https://URL_SUPABASE_ANDA.supabase.co'
-const supabaseKey = 'ANON_KEY_ANDA'
+const supabaseUrl = 'https://twunssclwdvwdbjwsysc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dW5zc2Nsd2R2d2RiandzeXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDExOTksImV4cCI6MjA5NTI3NzE5OX0.z_ME9KMif183URkPRs9JpRFhsudaNTLzojdmc7GuP1ceyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dW5zc2Nsd2R2d2RiandzeXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDExOTksImV4cCI6MjA5NTI3NzE5OX0.z_ME9KMif183URkPRs9JpRFhsudaNTLzojdmc7GuP1c'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
